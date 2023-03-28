d33173a1426cab64f87ff2427905ce6879f0a501
Rudolf_vh <matyane97@gmail.com>
2023-03-28 17:02:19 +0300

    Feature-Matvey

--- src/main/java/by/itacademy/git/mergeexperience/Module1Servlet.java
+++ src/main/java/by/itacademy/git/mergeexperience/Module1Servlet.java
@@ -23,33 +23,38 @@ public class Module1Servlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
 
         out.println("<table>");
+        out.println("<caption>Модуль 1</caption>");
         for (Module1Topics value : Module1Topics.values())
             if (value.getTopic().equals("GIT")) {
                 out.println("<tr><td>"
                         + value.getOrder() + "</td><td>"
                         + value.getTopic() + "</td><td>"
-                        + value.getDesc()
+                        + value.getDesc() + "</td><td>"
+                        + value.getHours()
                         + "</td><td><a href='https://prezi.com/view/ROBl3h39RTxz7DdBkRza/'>Подробнее</a>"
                         + "</td></tr>");
             } else if (value.getTopic().equals("Jakarta")) {
                 out.println("<tr><td>"
                         + value.getOrder() + "</td><td>"
                         + value.getTopic() + "</td><td>"
-                        + value.getDesc()
+                        + value.getDesc() + "</td><td>"
+                        + value.getHours()
                         + "</td><td><a href='https://jakarta.ee/'>Подробнее</a>"
                         + "</td></tr>");
             } else if (value.getTopic().equals("Apache Maven")) {
                 out.println("<tr><td>"
                         + value.getOrder() + "</td><td>"
                         + value.getTopic() + "</td><td>"
-                        + value.getDesc()
+                        + value.getDesc() + "</td><td>"
+                        + value.getHours()
                         + "</td><td><a href='https://maven.apache.org/'>Подробнее</a>"
                         + "</td></tr>");
             } else {
                 out.println("<tr><td>"
                         + value.getOrder() + "</td><td>"
                         + value.getTopic() + "</td><td>"
-                        + value.getDesc()
+                        + value.getDesc() + "</td><td>"
+                        + value.getHours()
                         + "</td><td><a href='not_implemented_yet.html'>Подробнее</a>"
                         + "</td></tr>");
             }
